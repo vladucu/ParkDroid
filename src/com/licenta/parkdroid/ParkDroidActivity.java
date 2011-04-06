653b08ab507df7ce79e72c056ac5561af51887a9
Vladucu <vanca.vlad@gmail.com>
2011-04-06 18:36:42 +0300

    created Android Project

---
+++ src/com/licenta/parkdroid/ParkDroidActivity.java
@@ -0,0 +1,13 @@
+package com.licenta.parkdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ParkDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
